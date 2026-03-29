448afa820157e3d04ac0e3791be14d6fd9a5c346
Klisman rDs <rds.systems90@gmail.com>
2026-03-29 01:47:01 -0300

    fix: Adicionar importação faltante de NotificacaoSistemaSerializer em views.py

--- backend/core/views.py
+++ backend/core/views.py
@@ -16,7 +16,7 @@ from .serializers import (
     StoreDetailSerializer, CategoriaSerializer, ProdutoSerializer,
     PedidoSerializer, FlexibleJSONField, GrupoDeAtributosSerializer,
     AtributoOpcaoSerializer, ItemPedidoSerializer, PerfilUsuarioLojaSerializer,
-    CaixaSerializer, MovimentacaoCaixaSerializer, UserSerializer, BairroEntregaSerializer
+    CaixaSerializer, MovimentacaoCaixaSerializer, UserSerializer, BairroEntregaSerializer, NotificacaoSistemaSerializer
 )
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
